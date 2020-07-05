a1d37ed5fa254cb182a133a73e4026fd495aeb27
shift02 <shift02ss@gmail.com>
2020-07-05 23:46:17 +0900

    htmlもbuildする処理を追加

--- packages/editor/src/main/ts/main/handler/BuilderHandler.ts
+++ packages/editor/src/main/ts/main/handler/BuilderHandler.ts
@@ -8,7 +8,7 @@ import {DevBuilder} from "../builder/DevBuilder";
  * ゲームを実行可能な形式に出力する処理をまとめているHandler
  */
 export class BuilderHandler {
-  
+
   public static registryICP(ipcMain: IpcMain) {
 
     //ローカルで実行するためのhtmlやjsファイルを生成
@@ -22,6 +22,7 @@ export class BuilderHandler {
         const project = window.getProject();
 
         await DevBuilder.buildJS(project.dir);
+        await DevBuilder.buildDevHtml(project.dir);
 
       }
 
