4dabe31193f5411e4cee10f03a02fd356277b20b
shift02 <shift02ss@gmail.com>
2020-07-07 00:01:31 +0900

    実行ボタンを押すとボタンの画像が変わるように変更

--- packages/editor/src/main/ts/renderer/components/application/mainapplication/MainApplication.tsx
+++ packages/editor/src/main/ts/renderer/components/application/mainapplication/MainApplication.tsx
@@ -2,29 +2,50 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {RunButton} from "../../button/runbutton/RunButton";
 import styles from "./main_application.css";
-import {RootStore} from "../../../stores/RootStore";
-import {Provider} from "react-redux";
+import {RootState, RootStore} from "../../../stores/RootStore";
+import {Provider, useSelector} from "react-redux";
+import {ReRunButton} from "../../button/rerunbutton/ReRunButton";
 
-export const MainApplication: React.FunctionComponent = (props): React.ReactElement => {
-
+//reduxを設定するためのラッパー
+const MainApplicationContainer: React.FunctionComponent = (props) => {
   return (
     <>
       <Provider store={RootStore}>
-        <div className={styles.main_container}>
-          <div className={styles.header}>
-            <RunButton/>
-          </div>
-          <div className={styles.main}>メインエディター</div>
-          <div className={styles.footer}>
-            ここに色々
-          </div>
-        </div>
+        <MainApplication/>
       </Provider>
+    </>
+  )
+};
+
+export const MainApplication: React.FunctionComponent = (props): React.ReactElement => {
+
+  const isRun = useSelector((state: RootState) => state.devGame.run);
+
+  const getRunButton = () => {
+    if (isRun) {
+      return <ReRunButton/>
+    } else {
+      return <RunButton/>
+    }
+
+  };
+
+  return (
+    <>
+      <div className={styles.main_container}>
+        <div className={styles.header}>
+          {getRunButton()}
+        </div>
+        <div className={styles.main}>メインエディター</div>
+        <div className={styles.footer}>
+          ここに色々
+        </div>
+      </div>
     </>
   );
 
 };
 
 export function createComponent() {
-  ReactDOM.render(<MainApplication/>, document.querySelector('#main'));
+  ReactDOM.render(<MainApplicationContainer/>, document.querySelector('#main'));
 }
---
+++ packages/editor/src/main/ts/renderer/components/button/rerunbutton/ReRunButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./re_run_button.css";
+
+export const ReRunButton: React.FunctionComponent = () => {
+
+  const click = async () => {
+
+  };
+
+  return (<>
+    <button className={styles.main} onClick={click}/>
+  </>)
+
+};
