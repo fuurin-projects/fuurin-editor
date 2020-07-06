0d74367a054e55d0ab11eb532f841e3410649917
shift02 <shift02ss@gmail.com>
2020-07-06 23:54:13 +0900

    runボタンでstateを切り替えるように

--- packages/editor/src/main/ts/renderer/components/application/mainapplication/MainApplication.tsx
+++ packages/editor/src/main/ts/renderer/components/application/mainapplication/MainApplication.tsx
@@ -2,20 +2,24 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {RunButton} from "../../button/runbutton/RunButton";
 import styles from "./main_application.css";
+import {RootStore} from "../../../stores/RootStore";
+import {Provider} from "react-redux";
 
 export const MainApplication: React.FunctionComponent = (props): React.ReactElement => {
 
   return (
     <>
-      <div className={styles.main_container}>
-        <div className={styles.header}>
-          <RunButton/>
+      <Provider store={RootStore}>
+        <div className={styles.main_container}>
+          <div className={styles.header}>
+            <RunButton/>
+          </div>
+          <div className={styles.main}>メインエディター</div>
+          <div className={styles.footer}>
+            ここに色々
+          </div>
         </div>
-        <div className={styles.main}>メインエディター</div>
-        <div className={styles.footer}>
-          ここに色々
-        </div>
-      </div>
+      </Provider>
     </>
   );
 
--- packages/editor/src/main/ts/renderer/components/button/runbutton/RunButton.tsx
+++ packages/editor/src/main/ts/renderer/components/button/runbutton/RunButton.tsx
@@ -2,15 +2,28 @@ import React from "react";
 import {BuilderRepository} from "../../../repository/BuilderRepository";
 import styles from "./run_button.css";
 import {GameRepository} from "../../../repository/GameRepository";
+import {useDispatch, useSelector} from "react-redux";
+import {RootState} from "../../../stores/RootStore";
+import {DevGameStore} from "../../../stores/DevGameStore";
 
 export const RunButton: React.FunctionComponent = () => {
 
+  const isRun = useSelector((state: RootState) => state.devGame.run);
+  const dispatch = useDispatch();
+
   const click = async () => {
+
+    dispatch(DevGameStore.actions.runGame({}));
+
     await BuilderRepository.instance().buildDev();
 
     await GameRepository.instance().runDev();
+
   };
 
-  return (<button className={styles.main} onClick={click}/>)
+  return (<>
+    <button className={styles.main} onClick={click}/>
+    {isRun && <>動作中</>}
+  </>)
 
 };
