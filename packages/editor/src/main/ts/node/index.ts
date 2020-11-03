73d28c92a7b330fedad490b0284aedf9f1c38fde
shift02 <shift02ss@gmail.com>
2020-11-03 23:42:42 +0900

    Reactの開発ツールを導入

--- packages/editor/src/main/ts/node/index.ts
+++ packages/editor/src/main/ts/node/index.ts
@@ -1,4 +1,5 @@
 import {app, ipcMain} from 'electron';
+import installExtension, {REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS} from 'electron-devtools-installer';
 import Configuration from "./Configuration";
 import ICPDispatcher from "./dispatcher/ICPDispatcher";
 import {WindowManager} from "./WindowManager";
@@ -12,7 +13,14 @@ async function main() {
   ICPDispatcher.registryICP(ipcMain);
 
   app.whenReady().then(async function () {
+
+    installExtension([REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS])
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log('An error occurred: ', err));
+
     WindowManager.instance().showLauncher();
+
+
   });
 
   ipcMain.on('test-message', (event, arg) => {
