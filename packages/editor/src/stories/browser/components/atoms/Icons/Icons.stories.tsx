1684bbd92ce69d2091a4ddbca9193e86cad92c78
shift02 <shift02ss@gmail.com>
2021-04-01 23:19:30 +0900

    SVGのストーリブックを追加

--- packages/editor/src/stories/browser/components/atoms/Icons/Icons.stories.tsx
+++ packages/editor/src/stories/browser/components/atoms/Icons/Icons.stories.tsx
@@ -1,23 +1,18 @@
-import {Meta, Story} from "@storybook/react/types-6-0";
-import React, {PropsWithChildren} from "react";
+import React from "react";
 import * as AllIcons from "../../../../../main/browser/components/atoms/Icons/Icons";
+import {storiesOf} from "@storybook/react";
 
-const template: React.FC<React.SVGProps<SVGSVGElement>> = () => {
-  return (
-    <svg></svg>
-  )
+//エクスポートされたコンポーネントを動的に管理するためにあえて storiesOf を使用
+const storyFnReactReturnTypeStoryApi = storiesOf("atoms/Icons", module);
+
+for (let allIconsKey in AllIcons) {
+
+  //@ts-ignore
+  const Icon = AllIcons[allIconsKey];
+
+  storyFnReactReturnTypeStoryApi
+    .add(allIconsKey, () => (
+      <Icon/>
+    ));
 }
 
-export default {
-  title: 'atoms/Icons',
-  component: template
-} as Meta;
-
-const Template: Story<PropsWithChildren<{}>> = (args) => (
-  <>{args.children}</>
-);
-
-export const Icon_TabClose = Template.bind({});
-Icon_TabClose.args = {
-  children: <AllIcons.IconTabClose/>
-};
